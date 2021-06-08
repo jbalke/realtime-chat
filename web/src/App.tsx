282fa1418044dfa5b056ded6600fdeaeb2958752
John Balke <john.balke@gmail.com>
2021-06-08 11:55:08 +0100

    Change client language to typescript

---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const App: FunctionComponent = (props) => (
+  <div>Hi there, I'm React from Webpack 5.</div>
+);
+
+export default App;
---
+++ web/webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.json'],
+  },
+
+  devServer: {
+    port: 8080,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'starter',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
