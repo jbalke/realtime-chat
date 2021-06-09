d6e03a955698ae02f424f3d22d13e0b92ad016eb
John Balke <john.balke@gmail.com>
2021-06-09 10:44:05 +0100

    Add subscription support

--- web/src/Chat.tsx
+++ web/src/Chat.tsx
@@ -1,17 +1,46 @@
 import {
   ApolloClient,
   ApolloProvider,
-  gql,
   InMemoryCache,
-  useMutation,
+  useMutation,,
 } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import React, { FunctionComponent, SyntheticEvent, useState } from 'react';
 import styled from 'styled-components';
 import MessageEntry from './components/MessageEntry';
 import Messages from './components/Messages';
 import { PostMessage, PostMessageVariables } from './generated/PostMessage';
 import { POST_MESSAGE } from './graphql/mutations/postMessage';
-import { Theme } from './styles/style-constants';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4001',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4001/subscriptions',
+  options: {
+    reconnect: true,
+  },
+});
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const Container = styled.div`
   width: 95%;
@@ -21,7 +50,7 @@ const Container = styled.div`
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4001/',
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
